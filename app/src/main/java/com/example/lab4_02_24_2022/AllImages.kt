df42014bed91adf54bdef6d254ffc83aa69db180
Kate <kate.piotrowski@greenwichschools.org>
2022-02-25 21:06:47 -0500

    First Commit

---
+++ app/src/main/java/com/example/lab4_02_24_2022/AllImages.kt
@@ -0,0 +1,21 @@
+package com.example.lab4_02_24_2022
+
+class AllImages {
+    public val allImages = arrayOf(
+        "https://images.metmuseum.org/CRDImages/eg/original/DT10852.jpg",
+        "https://images.metmuseum.org/CRDImages/mi/original/DP302645.jpg",
+        "https://images.metmuseum.org/CRDImages/ad/original/DT1816.jpg",
+        "https://images.metmuseum.org/CRDImages/ad/original/DT823.jpg",
+        "https://images.metmuseum.org/CRDImages/rl/original/DP324211.jpg",
+        "https://images.metmuseum.org/CRDImages/ad/original/DP218200.jpg"
+    )
+    public val allImageCaptions = arrayOf(
+        "Mummy Mask from A.D. 60–70 Egypt", //DP10852
+        "ex Kurtz Violin from 1560", //DP302645
+        "Honeycomb Quilt from 1830 by American artist Elizabeth Van Horne Clarkson", //DP1816
+        "Embroidered Carpet from 1832–35 Zeruah H. Guernsey Caswell made in Vermont", //DP823
+        "Design for a Wall Monument from 1490 by Italian artist Francesco di Giorgio Martini", //DP324211
+        "Eye of Maria Miles Heyward from 1802 by Edward Greene Malbone" //DP218200
+    )
+
+}
